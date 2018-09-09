f4b3651aa13cc6a9c745b448f7308121b6db8f9c
Ryan Scott <rainsworddu@gmail.com>
2018-09-10 00:22:13 +0800

    RENEW

--- random_numbers.py
+++ random_numbers.py
@@ -1,13 +1,17 @@
 import random
 r = random.randint(1, 100)
+count = 0 # 計數
 while True:
+    count += 1 # 等於 count = count + 1
     ques = input('guess number from 1~100: ')
     ques = int(ques)
     if ques == r:
         print('You are right!')
+        print('this is', count, 'times you guess')
         break
     else:
         if ques > r:
             print('your number is bigger than it')
         elif ques < r:
             print('your number is smaller than it')
+    print('this is', count, 'times you guess')
